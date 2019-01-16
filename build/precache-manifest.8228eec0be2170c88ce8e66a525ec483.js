24ed2eead6a928799a4f5733f34f869209664b22
Lars Nolden <larsnolden@web.de>
2019-01-16 15:58:37 -0500

    added build folder

---
+++ build/precache-manifest.8228eec0be2170c88ce8e66a525ec483.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1bcf34e7033808c7129046f1eb2ce67",
+    "url": "/static/media/stage5.d1bcf34e.svg"
+  },
+  {
+    "revision": "6331ad8244cc306b16e08dc9a31d8ef9",
+    "url": "/static/media/stage4.6331ad82.svg"
+  },
+  {
+    "revision": "860884f88ae740d9ddb121123ac2d106",
+    "url": "/static/media/stage3.860884f8.svg"
+  },
+  {
+    "revision": "dff07b3bd438f75fa3500921c178640d",
+    "url": "/static/media/stage2.dff07b3b.svg"
+  },
+  {
+    "revision": "ccf56b447bd76a95d90352adb9bd7d3d",
+    "url": "/static/media/stage1.ccf56b44.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7cd05ff5fdc1fe1b016e",
+    "url": "/static/js/main.7cd05ff5.chunk.js"
+  },
+  {
+    "revision": "19e2871e76fc5c0cd58d",
+    "url": "/static/js/1.19e2871e.chunk.js"
+  },
+  {
+    "revision": "7cd05ff5fdc1fe1b016e",
+    "url": "/static/css/main.07c8a754.chunk.css"
+  },
+  {
+    "revision": "c1f5faa99adc52c2cda8ab4a1e34cb10",
+    "url": "/index.html"
+  }
+];
